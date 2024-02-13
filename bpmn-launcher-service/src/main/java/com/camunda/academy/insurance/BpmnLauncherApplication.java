f5f879a17b3f4940d517d50b90b044018551c893
markklimenko <mark.klimenko.w@gmail.com>
2024-02-13 14:58:30 -0600

    Add persistence (in process)

--- bpmn-launcher-service/src/main/java/com/camunda/academy/insurance/BpmnLauncherApplication.java
+++ bpmn-launcher-service/src/main/java/com/camunda/academy/insurance/BpmnLauncherApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Deployment(resources = "classpath*:/bpmn/*.bpmn")
-public class Application {
+public class BpmnLauncherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(BpmnLauncherApplication.class, args);
     }
 }
